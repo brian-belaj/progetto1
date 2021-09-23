2cbcd83440f4933fd6d1a0b8b04b3cf71153c938
MirkoBoncompagni <91143819+MirkoBoncompagni@users.noreply.github.com>
2021-09-23 17:44:00 +0200

    roba
    aggiunto

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <!doctype html>
 <html lang="en">
   <head>
+      
+<link rel="manifest" href="/manifest.json">
+
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,7 +16,6 @@
   </head>
   <body>
     <header>
-        
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary flex-wrap">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
