120ab0e26b70c737b28d69ab6ff700fec2cf36c4
Brian <belaj.br@gmail.com>
2021-09-28 15:24:53 +0200

    In Partials Manifest
    Aggiunto shortcut e screenshots

--- list.php
+++ list.php
@@ -1,15 +1,7 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<?php require 'partials/head.php'?>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-    <title>Hello, world!</title>
-  </head>
   <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary flex-wrap">
@@ -361,13 +353,7 @@
             <!-- ShareThis BEGIN --><div class="mt-5 sharethis-inline-share-buttons"></div><!-- ShareThis END -->
         </div>
     </footer>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+    <?php require 'partials/script.php'?>
 
-    <script
-      type="text/javascript"
-      src="https://platform-api.sharethis.com/js/sharethis.js#property=61309f70a855ee0013f55721&product=sop"
-      async="async"
-    ></script>
   </body>
 </html> 
