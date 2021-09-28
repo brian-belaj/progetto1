a31d11f5e7f60d15da34e9394a69cf63bb5ffd34
Brian <belaj.br@gmail.com>
2021-09-28 16:22:58 +0200

    APP.JS UPDATE
    aggiornamento del js app

--- js/app.js
+++ js/app.js
@@ -7,3 +7,30 @@ if('serviceWorker' in navigator){
         console.log(error);
     });
 }
+
+var deferredprompt;
+window.addEventListener("beforeinstallprompt", function(event){
+    event.preventDefault();
+    console.log("intercettato evento di installazione");
+    deferredprompt=event;
+    return false;
+});
+var buttons = document.querySelectorAll(".btn-primary");
+for (var index; index<buttons.length; index++){
+    buttons[index].addEventListener("click", function(event){
+        event.preventDefault();
+        if(deferredprompt){
+            deferredprompt.prompt();
+            deferredprompt.userChoice.then(
+                function(choiceresult){
+                    if(choiceresult.outcome === "dismissed"){
+                        console.log("l'utente ha annullato l'installazione");
+                    } else {
+                        console.log("l'utente ha installato l'app");
+                    }
+                    window.location.href=event.target.getAttribute("href");
+                })
+            deferredprompt=null;
+        }
+    });
+}
