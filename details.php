5c4966f22e698ce42b5ae001b8a71a71a949bd53
Alessio Lombardini <alessio.lombardini@alias2k.com>
2021-10-07 11:32:05 +0200

    Update details.php

--- details.php
+++ details.php
@@ -16,33 +16,33 @@
     <?php require 'partials/header.php'?>
 
         <main>
-            <section class="mt-5 container">
+        <section class="mt-5 container">
                 <article class="row gx-0">
                     <div class="col-12 col-lg-6">
                         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                             <div class="carousel-inner mb-2">
                                 <div class="carousel-item active">
-                                        <img src="https://picsum.photos/635/400/?random&t=1" class="d-block w-100" alt="...">
+                                        <img src="/images/immagini/IMG_4155.jpg" class="d-block w-100" alt="...">
                                 </div>
                                 <div class="carousel-item">
-                                        <img src="https://picsum.photos/635/400/?random&t=2" class="d-block w-100" alt="...">
+                                        <img src="/images/immagini/IMG_4155a.jpg" class="d-block w-100" alt="...">
                                 </div>
                                 <div class="carousel-item">
-                                        <img src="https://picsum.photos/635/400/?random&t=3" class="d-block w-100" alt="...">
+                                        <img src="/images/immagini/IMG_4155.jpg" class="d-block w-100" alt="...">
                                 </div>
                             </div>
                             <div class="d-flex flex-row">
                                 <a data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
                                     class="active me-2" aria-current="true" aria-label="Slide 1">
-                                    <img src="https://picsum.photos/635/400/?random&t=1" class="d-block w-100" alt="...">
+                                    <img src="/images/immagini/IMG_4155.jpg" class="d-block w-100" alt="...">
                                 </a>
                                 <a data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
                                     aria-label="Slide 2" class="me-2">
-                                    <img src="https://picsum.photos/635/400/?random&t=2" class="d-block w-100" alt="...">
+                                    <img src="/images/immagini/IMG_4155a.jpg" class="d-block w-100" alt="...">
                                 </a>
                                 <a data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
                                     aria-label="Slide 3"">
-                                    <img src="https://picsum.photos/635/400/?random&t=3" class="d-block w-100" alt="...">
+                                    <img src="/images/immagini/IMG_4155.jpg" class="d-block w-100" alt="...">
                                 </a>
                             </div>
                         </div>
