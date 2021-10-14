f8aea95c64f34edbb80c6e906b889511f422759e
dhhdff <dhh_dff@yahoo.it>
2021-10-14 18:03:11 +0200

    Update _db_connection.php

--- partials/_db_connection.php
+++ partials/_db_connection.php
@@ -6,8 +6,10 @@ $user = "root";
 $pass = "password";
 
 try {
-    $pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $user, $pass);
-} catch (PDOException $e) {
-    echo "Errore: " . $e->getMessage();
-    die();
-}
+        $pdo = new PDO("mysql:host=$hostname;dbname=$database_name", $user, $pass, [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ]);
+    } catch (PDOException $e) {
+        echo "Errore: " . $e->getMessage();
+        die();
+    }
