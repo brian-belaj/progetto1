fd82137b87cc635eef051adc2f20f53eb2344578
Brian <belaj.br@gmail.com>
2021-10-21 13:20:15 +0200

    aggiornamento esercizio

--- partials/_website_data.php
+++ partials/_website_data.php
@@ -92,14 +92,15 @@ if(isset($_GET['article_id'])){
     $sth->execute();
 
     $article_detail = $sth->fetch(\PDO::FETCH_ASSOC);
-
     define('ARTICLEDETAIL', $article_detail);
 //var_dump(ARTICLEDETAIL);
 $sth = $pdo->prepare("SELECT * FROM articles_photo WHERE articles_id = $article_id");
 $sth->execute();
+$article_photos = $sth->fetchAll(\PDO::FETCH_ASSOC);
+$sth = $pdo->prepare("SELECT * FROM articles_photo WHERE articles_id = $article_id LIMIT 1");
+$sth->execute();
 
 $article_photos = $sth->fetchAll(\PDO::FETCH_ASSOC);
 
-$sth = $pdo->prepare("SELECT * FROM articles_photo ORDER BY articles_id LIMIT 1");
-$sth->execute();
+
 }
